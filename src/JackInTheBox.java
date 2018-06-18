d3e4a25fa8b6365c5bbcbdc1f424288c596da5e4
catsrock <league@wts21.attlocal.net>
2018-06-17 17:31:09 -0700

    jack in the box ip

--- src/JackInTheBox.java
+++ src/JackInTheBox.java
@@ -1,4 +1,37 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JackInTheBox {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class JackInTheBox implements ActionListener{
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	
+public static void main(String[] args) {
+	JackInTheBox jack = new JackInTheBox();
+	jack.showButton();
+}
+
+
+private void showButton() {
+	// TODO Auto-generated method stub
+	frame.setVisible(true);
+	frame.add(panel);
+	frame.add(button);
+	panel.add(button);
+	frame.pack();
+	button.setText("Surprise");
+	button.addActionListener(this);
+	
+}
+public void actionPerformed(ActionEvent e) {
+	if(button
+	
+	
+	
+
+}
 }
